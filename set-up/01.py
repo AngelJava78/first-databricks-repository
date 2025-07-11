6301d58b26791edb6df65601ea4013b023b0fbae
AngelJava78 <AngelJava78>
2025-07-11 20:12:13 +0000

    Read csv file

--- set-up/01.py
+++ set-up/01.py
@@ -14,3 +14,15 @@ display(dbutils.fs.ls("/"))
 
 accessKey=dbutils.secrets.get(scope="kvs-accessKey-dbx-dev-eastus", key="kvs-accessKey-dbx-dev-eastus")
 print(accessKey)
+
+# COMMAND ----------
+
+spark.conf.set("fs.azure.account.key.stdbxdeveastus01.dfs.core.windows.net", accessKey)
+
+# COMMAND ----------
+
+display(dbutils.fs.ls("abfss://demo@stdbxdeveastus01.dfs.core.windows.net"))
+
+# COMMAND ----------
+
+display(spark.read.csv("abfss://demo@stdbxdeveastus01.dfs.core.windows.net/movie.csv"))
